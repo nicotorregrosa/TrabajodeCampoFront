97dc10f6934ba43d8a5091a4ca52936fc4b0d3be
nico <nico_torregrosa@hotmail.com>
2024-04-28 02:40:02 -0300

    agrego mas rutas

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Componentes/permanentes/Navbar';
 import Inicio from './Componentes/paginas/inicio/Inicio';
 import Aulas from './Componentes/paginas/aulas/Aulas';
+import TipoDeEspacio from './Componentes/paginas/tipo de espacio/TipoDeEspacio';
 import Carreras from './Componentes/paginas/carreras/Carreras';
 import Elementos from './Componentes/paginas/elementos/Elementos';
 import Espacios from './Componentes/paginas/espacios/Espacios';
 import Materias from './Componentes/paginas/materias/Materias';
 import Personal from './Componentes/paginas/personal/Personal';
 import PlanDeEstudio from './Componentes/paginas/plan de estudio/PlanDeEstudio';
-import TipoDeEspacio from './Componentes/paginas/tipo de espacio/TipoDeEspacio';
+import Footer from './Componentes/permanentes/Footer';
+
 
 const App = () => {
     return (
@@ -18,16 +20,17 @@ const App = () => {
             <main>
                 <Routes>
                     <Route exact path='/' element={<Inicio></Inicio>} ></Route>
-                    <Route exact path='/' element={<Aulas></Aulas>} ></Route>
-                    <Route exact path='/' element={<Carreras></Carreras>} ></Route>
-                    <Route exact path='/' element={<Elementos></Elementos>} ></Route>
-                    <Route exact path='/' element={<Espacios></Espacios>} ></Route>
-                    <Route exact path='/' element={<Materias></Materias>} ></Route>
-                    <Route exact path='/' element={<Personal></Personal>} ></Route>
-                    <Route exact path='/' element={<PlanDeEstudio></PlanDeEstudio>} ></Route>
-                    <Route exact path='/' element={<TipoDeEspacio></TipoDeEspacio>} ></Route>
+                    <Route exact path='/Aulas' element={<Aulas></Aulas>} ></Route>
+                    <Route exact path='/Carreras' element={<Carreras></Carreras>} ></Route>
+                    <Route exact path='/Elementos' element={<Elementos></Elementos>} ></Route>
+                    <Route exact path='/Espacios' element={<Espacios></Espacios>} ></Route>
+                    <Route exact path='/Materias' element={<Materias></Materias>} ></Route>
+                    <Route exact path='/Personal' element={<Personal></Personal>} ></Route>
+                    <Route exact path='/PlanDeEstudio' element={<PlanDeEstudio></PlanDeEstudio>} ></Route>
+                    <Route exact path='/TipoDeEspacio' element={<TipoDeEspacio></TipoDeEspacio>} ></Route>
                 </Routes>
             </main>
+            <Footer></Footer>
         </Router>
     );
 };
